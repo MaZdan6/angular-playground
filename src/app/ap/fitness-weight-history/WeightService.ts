c8dbac7c6ac49b3d880b4ff3deabe29619a9b0fe
Mateusz <mateusz.6.zdanowicz@gmail.com>
2020-10-04 22:18:50 +0200

    pagination

--- src/app/ap/fitness-weight-history/WeightService.ts
+++ src/app/ap/fitness-weight-history/WeightService.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable, Subject, throwError} from 'rxjs';
-import {catchError, tap, map} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 import {Weight} from './Weight';
 
